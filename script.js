38f294f344515c9dea35f14742eb1740d7bfb78c
Andy Lee <freerangeandy@gmail.com>
2020-06-17 15:49:00 -0400

    clicking on cell shows input field to change base currency value

--- script.js
+++ script.js
@@ -29,7 +29,6 @@ const createCells = (currencyCodes) => {
   });
 }
 
-
 const loadCurrencies = () => {
   fetch(`${EXCHANGERATE_URL}${API_KEY}/latest/USD`)
   .then(response => {
@@ -57,37 +56,82 @@ const loadRates = (conversionRates) => {
 
 }
 
-const updateDisplayRates = (baseCode) => {
+const updateDisplayRates = (baseCode, baseMultiplier) => {
   const otherCurrencyListItems = Array.from(document.getElementsByClassName("hidden-item"))
   let newCurrencyRates = {}
   const baseRate = document.getElementById(baseCode).innerHTML
   otherCurrencyListItems.forEach(node => {
     const otherCode = node.id
     const newRate = parseFloat(node.innerHTML) / parseFloat(baseRate)
-    newCurrencyRates[node.id] = newRate
+    newCurrencyRates[node.id] = newRate * parseFloat(baseMultiplier)
   })
-
-  setDisplayRates(newCurrencyRates, baseCode)
+  displayNewRates(newCurrencyRates, baseCode)
 }
 
-const setDisplayRates = (allCurrencyRates, baseCode) => {
+const displayNewRates = (allCurrencyRates, baseCode) => {
   Object.entries(allCurrencyRates).forEach(([code, rate]) => {
-    const currencyRate = document.getElementById(`${code}-cell-value`)
-    currencyRate.innerHTML = rate.toFixed(3)
+    if (code != baseCode) {
+      const currencyRate = document.getElementById(`${code}-cell-value`)
+      currencyRate.innerHTML = rate.toFixed(3)
+    }
   })
 }
 
 const getClickHandler = (currencyCode) => {
   return (event) => {
-    const previousSelected = currencyTable.querySelector('.selected-cell')
-    if (previousSelected) previousSelected.className = 'currency-cell'
     const currencyCell = document.getElementById(`${currencyCode}-cell`)
-    currencyCell.className = 'selected-cell'
-    const currencyCellRate = document.getElementById(`${currencyCode}-cell-value`)
-    currencyCellRate.innerHTML = "1.00"
-    // console.log(currencyCode + " was clicked")
-    updateDisplayRates(currencyCode)
+    if (currencyCell.className !== 'selected-cell') {
+      revertPreviousSelection()
+      const currencyCell = document.getElementById(`${currencyCode}-cell`)
+      currencyCell.className = 'selected-cell'
+      const cellChildren = currencyCell.childNodes
+
+      if (cellChildren.length >= 2){
+        const baseInput = createBaseInput(currencyCode)
+        currencyCell.removeChild(cellChildren[1])
+        currencyCell.appendChild(baseInput)
+      }
+    }
+    const baseMultiplier = getMultiplier()
+    updateDisplayRates(currencyCode, baseMultiplier)
   }
+}
+
+const revertPreviousSelection = () => {
+    const previousSelected = currencyTable.querySelector('.selected-cell')
+    if (previousSelected) {
+      const baseInput = document.getElementById('base-input')
+      previousSelected.removeChild(baseInput)
+      previousSelected.className = 'currency-cell'
+      const currencyRate = document.createElement("p")
+      const currencyRateText = document.createTextNode("")
+      currencyRate.appendChild(currencyRateText)
+      const code = previousSelected.firstChild.innerHTML
+      currencyRate.id = `${code}-cell-value`
+      previousSelected.appendChild(currencyRate)
+    }
+}
+
+const createBaseInput = (baseCode) => {
+  const typeAttr = document.createAttribute("type")
+  typeAttr.value = "number"
+  const minValueAttr = document.createAttribute("min")
+  minValueAttr.value = 1
+
+  const inputNode = document.createElement("input")
+  inputNode.setAttributeNode(typeAttr)
+  inputNode.setAttributeNode(minValueAttr)
+  inputNode.id = 'base-input'
+  inputNode.className = 'base-input'
+  inputNode.value = 1
+  inputNode.addEventListener("change", () => updateDisplayRates(baseCode, inputNode.value))
+  return inputNode
+}
+
+const getMultiplier = () => {
+  const multiplierNode = document.getElementById('base-input')
+  if (!multiplierNode || parseFloat(multiplierNode) === NaN) return 1
+  return multiplierNode.value
 }
 
 loadCurrencies()
